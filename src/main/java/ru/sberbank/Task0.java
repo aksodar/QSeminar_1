fe9884b5e62213aaa558a16e4073194661e9613c
Elena Polozova <EVPolozova@sberbank.ru>
2023-01-22 15:06:42 +0700

    Полозова Елена - домашнее задание номер 1

--- src/main/java/ru/sberbank/Task0.java
+++ src/main/java/ru/sberbank/Task0.java
@@ -6,6 +6,14 @@ public class Task0 {
     // Создать класс Developer
     // в методе продемонстрировать создание объектов указанных классов и вывести в консоль их параметры
     public static void main(String[] args) {
+        int id = 5;
+        boolean isDeveloped = true;
+        boolean isTested = true;
+
+        Task TaskNew = new Task(id, isDeveloped, isTested);
+        System.out.println(TaskNew.id);
+        System.out.println(TaskNew.isDeveloped);
+        System.out.println(TaskNew.isTested);
 
     }
 
--- src/main/java/ru/sberbank/Task1.java
+++ src/main/java/ru/sberbank/Task1.java
@@ -5,6 +5,42 @@ public class Task1 {
     // Модифицировать класс Developer
     // в методе продемонстрировать реализованную логику
     public static void main(String[] args) {
+        int id = 1;
+
+        Developer DevNew = new Developer(id);
+        System.out.println(DevNew.id);
+        System.out.println(DevNew.isFree);
+
+        Task TaskNew = new Task(id, false, false);
+        DevNew.addTask(TaskNew);
+        System.out.println(DevNew.id);
+        System.out.println(DevNew.isFree);
+
+        DevNew.makeTask();
+        System.out.println(DevNew.id);
+        System.out.println(DevNew.isFree);
+
+        DevNew.release();
+        System.out.println(DevNew.id);
+        System.out.println(DevNew.isFree);
+
+        //Тестируем задачу
+
+        Tester Test = new Tester(id);
+        System.out.println(Test.id);
+        System.out.println(Test.isFree);
+
+        Test.addTask(TaskNew);
+        System.out.println(Test.id);
+        System.out.println(Test.isFree);
+
+        Test.checkTask();
+        System.out.println(Test.id);
+        System.out.println(Test.isFree);
+
+        Test.release();
+        System.out.println(Test.id);
+        System.out.println(Test.isFree);
 
     }
 
